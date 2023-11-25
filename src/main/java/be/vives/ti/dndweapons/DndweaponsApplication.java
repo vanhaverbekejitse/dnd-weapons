d052f28516a4eb413c3e969c633478f9ccc0d60b
Jitse Vanhaverbeke <r0890652@student.vives.be>
2023-11-25 15:49:04 +0100

    first commit

---
+++ src/main/java/be/vives/ti/dndweapons/DndweaponsApplication.java
@@ -0,0 +1,13 @@
+package be.vives.ti.dndweapons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndweaponsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndweaponsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/vives/ti/dndweapons/DndweaponsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vives.ti.dndweapons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndweaponsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
